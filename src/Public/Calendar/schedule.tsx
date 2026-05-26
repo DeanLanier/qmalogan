5dd0184c624a8a1d74dd73fb1658c682feaf502c
Dean Lanier <dean.lanier@gmail.com>
2026-05-25 18:30:52 -0600

    Remove extra spaces

--- src/Public/Calendar/schedule.tsx
+++ src/Public/Calendar/schedule.tsx
@@ -22,7 +22,7 @@ export function Schedule() {
                         {
                             day: "Tuesday",
                             events: [
-                                "3:00 PM - Kyuki-do / Homeschool  (Ages 5-12)",
+                                "3:00 PM - Kyuki-do / Homeschool (Ages 5-12)",
                                 "4:00 PM - Kyuki-do / Little Tigers (Ages 3-5)",
                                 "5:00 PM - Kyuki-do / Kids (Ages 6-12)",
                                 "6:00 PM - Kyuki-do / Adults (Ages 12+)",
@@ -39,7 +39,7 @@ export function Schedule() {
                         {
                             day: "Thursday",
                             events: [
-                                "3:00 PM - Kyuki-do / Homeschool  (Ages 5-12)",
+                                "3:00 PM - Kyuki-do / Homeschool (Ages 5-12)",
                                 "4:00 PM - Kyuki-do / Little Tigers (Ages 3-5)",
                                 "5:00 PM - Kyuki-do / Kids (Ages 6-12)",
                                 "6:00 PM - Kyuki-do / Adults (Ages 12+)",
